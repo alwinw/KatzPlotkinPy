9415c3555cf42de9d3678f39e85a395d1294f2f3
alwinw <16846521+alwinw@users.noreply.github.com>
2022-08-09 22:23:17 +1000

    feat(afgen): testing cp calc

--- src/katzplotkinpy/afgen.py
+++ src/katzplotkinpy/afgen.py
@@ -4,10 +4,93 @@ import numpy as np
 
 logger = logging.getLogger(__name__)
 
-# TODO: Implement Joukowski Transformation
+# TODO: Implement Joukowski Transformation Airfoil
 
 
-def van_de_vooren(epsilon: float, k: float, alpha: float, m: int):
+def van_de_vooren_imag(epsilon: float, k: float, alpha: float, m: int):
+    """Generates van de Vooren Airfoil. Refer to Section 6.7
+
+    :param epsilon: Thickness coefficient (between 0 and 1)
+    :type epsilon: float
+    :param k: Trailing edge angle coefficient
+    :type k: float
+    :param alpha: Angle of attack (deg)
+    :type alpha: float
+    :param m: Number of panels
+    :type m: int
+    """
+
+    # Parameters
+    l = 1.0  # Half chord of the airfoil
+    a = 2 * l * np.power(epsilon + 1, k - 1) / np.power(2, k)  # Eq. 6.65
+    theta = np.linspace(0, 2 * np.pi, m)
+    # tau = np.pi * (2 - k)
+
+    # Eq 6.74a and 6.74b
+    r1 = np.sqrt(np.power(a * np.cos(theta) - a, 2) + np.power(a * np.sin(theta), 2))
+    r2 = np.sqrt(
+        np.power(a * np.cos(theta) - epsilon * a, 2) + np.power(a * np.sin(theta), 2)
+    )
+    theta1 = np.arctan2(a * np.sin(theta), a * np.cos(theta) - a)
+    theta2 = np.arctan2(a * np.sin(theta), a * np.cos(theta) - epsilon * a)
+
+    x = (
+        np.power(r1, k)
+        / np.power(r2, k - 1)
+        * (
+            np.cos(k * theta1) * np.cos((k - 1) * theta2)
+            + np.sin(k * theta1) * np.sin((k - 1) * theta2)
+        )
+        + l
+    )
+    z = (
+        np.power(r1, k)
+        / np.power(r2, k - 1)
+        * (
+            np.sin(k * theta1) * np.cos((k - 1) * theta2)
+            - np.cos(k * theta1) * np.sin((k - 1) * theta2)
+        )
+    )
+
+    Qinf = 1
+    alpha = np.deg2rad(alpha)
+
+    A = np.cos((k - 1) * theta1) * np.cos(k * theta2) + np.sin(
+        (k - 1) * theta1
+    ) * np.sin(k * theta2)
+    B = np.sin((k - 1) * theta1) * np.cos(k * theta2) - np.cos(
+        (k - 1) * theta1
+    ) * np.sin(k * theta2)
+
+    D0 = a * (1 - k + k * epsilon)
+    D1 = A * (a * np.cos(theta) - D0) + B * (a * np.sin(theta))
+    D2 = A * (a * np.sin(theta)) + B * (a * np.cos(theta) - D0)
+
+    u = (
+        2
+        * Qinf
+        * np.power(r2, k)
+        / np.power(r1, k - 1)
+        * (np.sin(alpha) - np.sin(alpha - theta))
+        / (np.power(D1, 2) + np.power(D2, 2))
+        * (D1 * np.sin(theta) + D2 * np.cos(theta))
+    )
+    w = (
+        -2
+        * Qinf
+        * np.power(r2, k)
+        / np.power(r1, k - 1)
+        * (np.cos(alpha) - np.cos(alpha - theta))
+        / (np.power(D1, 2) + np.power(D2, 2))
+        * (D1 * np.cos(theta) - D2 * np.sin(theta))
+    )
+
+    CP = 1 - (np.power(u, 2) + np.power(w, 2)) / np.power(Qinf, 2)  # Eq 6.70
+
+    return x, z
+
+
+def van_de_vooren_imag(epsilon: float, k: float, alpha: float, m: int):
     """Generates van de Vooren Airfoil. Refer to Section 6.7
 
     :param epsilon: Thickness coefficient (between 0 and 1)
@@ -24,8 +107,8 @@ def van_de_vooren(epsilon: float, k: float, alpha: float, m: int):
     # )
 
     # Parameters
-    len = 1.0  # Half chord of the airfoil
-    a = 2 * len * np.power(epsilon + 1, k - 1) / np.power(2, k)  # Eq. 6.65
+    l = 1.0  # Half chord of the airfoil
+    a = 2 * l * np.power(epsilon + 1, k - 1) / np.power(2, k)  # Eq. 6.65
     theta = np.linspace(0, 2 * np.pi, m)
     # tau = np.pi * (2 - k)
 
@@ -34,10 +117,11 @@ def van_de_vooren(epsilon: float, k: float, alpha: float, m: int):
     # Alternatively equations 6.74a and 6.74b could be used on the real plane
     circle = a * np.cos(theta) + a * 1j * np.sin(theta)
     airfoil = (
-        np.power(circle - a, k) / np.power(circle - a * epsilon, k - 1) + len
+        np.power(circle - a, k) / np.power(circle - a * epsilon, k - 1) + l
     )  # Eq 6.62
 
     # TODO: airfoil coordinate transformation with alpha
+    alpha = np.deg2rad(alpha)
 
     return airfoil.real, airfoil.imag
 
--- tests/f77/afgenvis.py
+++ tests/f77/afgenvis.py
@@ -10,6 +10,7 @@ log = logging.getLogger(__name__)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("afgen_file", metavar="afgen_file", type=str, help="afgen file")
+    parser.add_argument("cp_file", metavar="cp_file", type=str, help="cp file")
     args = parser.parse_args()
 
     logging.basicConfig(
@@ -18,11 +19,10 @@ if __name__ == "__main__":
     )
     log.info(f"Reading AFGEN file: {args.afgen_file}")
 
-    afgen = pd.read_csv(args.afgen_file, sep=",", names=["x", "z"], header=None)
-    # print(afgen)
+    afoil = pd.read_csv(args.afgen_file, sep=",", names=["x", "z"], header=None)
 
-    plt = (
-        pn.ggplot(afgen, pn.aes("x", "z"))
+    af_plt = (
+        pn.ggplot(afoil, pn.aes("x", "z"))
         + pn.geom_point()
         + pn.geom_path()
         + pn.coord_fixed(xlim=(-1.25, 1.25), ylim=(-1, 1))
@@ -34,4 +34,19 @@ if __name__ == "__main__":
         + pn.scale_y_continuous(breaks=np.arange(-1, 1.25, 0.25), expand=(0, 0))
         + pn.theme_bw()
     )
-    print(plt)
+
+    log.info(f"Reading CP file: {args.cp_file}")
+    cp = pd.read_csv(args.cp_file, sep=",", names=["x", "cp"], header=None)
+
+    cp_plt = (
+        pn.ggplot(cp, pn.aes("x", "cp"))
+        + pn.geom_point()
+        + pn.geom_path()
+        + pn.scale_x_continuous(
+            breaks=np.linspace(-1, 1, 5),
+            minor_breaks=np.arange(-1.25, 1.5, 0.25),
+            expand=(0, 0),
+        )
+        + pn.theme_bw()
+    )
+    print(cp_plt)
